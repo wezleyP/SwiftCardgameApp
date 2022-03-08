f2d5553afd1cea8ac754186cab61b0c044a09b3a
wezleyp <wesley.patterson121@gmail.com>
2022-03-07 21:54:16 -0600

    Initial Commit

---
+++ gameApp/gameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameAppApp.swift
+//  gameApp
+//
+//  Created by Wesley Patterson on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct gameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
